a62a878470f45b73af33498f08d1d74fb13996f0
maciej.meijer <maciej.meijer@tieto.com>
2019-10-16 16:33:58 +0200

    emulator_8080: add CLI Functionality

--- main_file.c
+++ main_file.c
@@ -46,16 +46,95 @@ static int load_rom_memory_from_file(const char* rom_file, memory_t* rom_mem)
 	return 0;
 }
 
+static void create_ram_memory()
+{
+	ram_memory.memory = (uint8_t*) malloc(RAM_SIZE);
+	ram_memory.memory_size = RAM_SIZE;
+
+	if(ram_memory.memory == NULL) {
+		printf("ERROR: Failed to allocate RAM memory\n");
+		exit(0);
+	}
+}
+
 static int create_test_rom_image(memory_t* rom_mem)
 {
 	uint8_t memcpy_opcode[] = { 0x78, 0xB1, 0xC8, 0x1A, 0x77, 0x13, 0x23, 0x0B, 0x78, 0xB1, 0xC2, 0x03, 0x00, 0xC9 };
-	// uint8_t memcpy_opcode[] = {0x00, 0x01, 0x00, 0x01};
-	
 
 	rom_mem->memory = malloc(sizeof(memcpy_opcode));
 	rom_mem->memory_size = sizeof(memcpy_opcode);
 
 	memcpy(rom_mem->memory, memcpy_opcode, sizeof(memcpy_opcode));
+}
+
+
+static bool get_opcode(memory_t* mem, char* ass_code)
+{
+	memset(mem->memory, 0, 4);
+
+	mem->memory[0] = 0x04;
+	mem->memory_size = 1;
+
+	return 1;
+}
+
+static void enter_cli()
+{
+	uint8_t bLoop = 0;
+
+	rom_memory.memory = malloc(4);
+	rom_memory.memory_size = 0;
+
+	while(bLoop == 0) {
+		char str[80] = {0};
+
+		printf("Assembly Code << ");
+		int ret = scanf(" %[^\n]s", str);
+
+		if (get_opcode(&rom_memory, str)) {
+			execute_cpu(&ram_memory, &rom_memory);
+		}
+	}
+}
+
+static void enter_bin(char* rom_file)
+{
+	if(load_rom_memory_from_file(rom_file, &rom_memory) != 0)   {
+		exit(0);
+	}
+
+	execute_cpu(&ram_memory, &rom_memory);
+}
+
+static void enter_test_image() {
+
+	create_ram_memory();
+	create_test_rom_image(&rom_memory);
+
+	execute_cpu(&ram_memory, &rom_memory);
+}
+
+static void execute_code(char exec, int argc, char* argv[])
+{
+	if (exec == 'c') {
+		printf("8080 Emulator Cli Mode\n");
+		enter_cli();
+	}
+
+	else if(exec == 'b' && argc >= 3) {
+		printf("8080 Emulator Normal Mode\n");
+		enter_bin(argv[2]);
+	}
+
+	else if(exec == 't' ) {
+		printf("8080 Emulator Test Image Mode\n");
+		enter_test_image();
+	}
+
+	else {
+		printf("Invalid arguments provided\n");
+		exit(0);
+	}
 }
 
 int main(int argc, char* argv[])
@@ -64,27 +143,15 @@ int main(int argc, char* argv[])
 
 	if(argc >= 2)   {
 	
-		if(load_rom_memory_from_file(argv[1], &rom_memory) != 0)   {
+		if(argv[1][0] != '-') {
+			printf("Error No arguments provided\n");
 			exit(0);
 		}
+
+		create_ram_memory();
+		execute_code(argv[1][1], argc, argv);
 	}
 
-	else {
-		printf("ERROR: No ROM Image provided\n");
-
-		create_test_rom_image(&rom_memory);
-	}
-
-
-	ram_memory.memory = (uint8_t*) malloc(RAM_SIZE);
-	ram_memory.memory_size = RAM_SIZE;
-
-	if(ram_memory.memory == NULL) {
-		printf("ERROR: Failed to allocate RAM memory\n");
-		exit(0);
-	}
-
-	execute_cpu(&ram_memory, &rom_memory);
 
 	return 0;
 }
